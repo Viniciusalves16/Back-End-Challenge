2c7916cd6cea7d0255b12231b14baeaf34ec435b
Vinicius Alves <103587422+Viniciusalves16@users.noreply.github.com>
2024-08-28 21:33:42 -0300

    criação de rota principal que gerencia as chamadas através dos verbos (post/get/put/delete)

--- Back-End-Challenge-Contas-Correntes/src/main/java/com/example/account/route/CustomerRegistrationRoute.java
+++ Back-End-Challenge-Contas-Correntes/src/main/java/com/example/account/route/CustomerRegistrationRoute.java
@@ -1,26 +1,44 @@
 
 package com.example.account.route;
 
-import com.example.account.enumeration.RouteEnumeration;
+import com.example.account.model.Customer;
+import com.example.account.record.CustomerRecord;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomerRegistrationRoute extends RouteBuilder {
-
     @Override
     public void configure() throws Exception {
-        restConfiguration()
+        restConfiguration().
+                apiContextRouteId("swagger")
+                .contextPath("/contas/v1")
                 .component("servlet")
-                .bindingMode(RestBindingMode.auto);
+                .host("localhost")
+                .port(8080)
+                .scheme("http");
 
-        rest().path("account")
-                .produces("application/json")
-                .post("/registration")
+
+        rest().path("/registration")
+                .post()
+                .id("createCustomer")
+                .bindingMode(RestBindingMode.auto)
+                .skipBindingOnErrorCode(true)
+                .type(CustomerRecord.class)
                 .consumes("application/json")
-                .type(Account)
-                .to(RouteEnumeration.REGISTRATION.getId());
+                .produces("application/json")
+                .to("direct:registration");
+
+        from("direct:registration")
+                .log("Received customer: ${body}")
+                .process(exchange -> {
+                    Customer customer = exchange.getIn().getBody(Customer.class);
+                    // Process customer data here
+                    System.out.println("Customer received: " + customer);
+                })
+                .setBody(simple("Customer ${body.name} processed successfully"))
+                .end();
 
     }
 }
--- Back-End-Challenge-Contas-Correntes/src/main/java/com/example/account/route/Registration/CustomerRoute.java
+++ Back-End-Challenge-Contas-Correntes/src/main/java/com/example/account/route/Registration/CustomerRoute.java
@@ -1,15 +1,15 @@
-//package com.example.account.route.Registration;
-//
-//import com.example.account.enumeration.RouteEnumeration;
-//import org.apache.camel.Route;
-//import org.apache.camel.builder.RouteBuilder;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class CustomerRoute extends RouteBuilder {
-//    @Override
-//    public void configure() throws Exception {
-//        from(RouteEnumeration.REGISTRATION.getId())
-//                .log("Inicio do cadastro do cliente")
-//    }
-//}
+package com.example.account.route.Registration;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("direct:registration")
+                .process(Exchange -> System.out.println("cheguei ate aqui"))
+                .log("Inicio do cadastro do cliente").end();
+    }
+}
